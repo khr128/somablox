ff7897cc655cec078fc7601d77dcbf6bd08b640e
Dmitri Khrebtukov <khr128@gmail.com>
2012-07-14 14:27:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Somablox::Application.config.secret_token = 'f13613511a6f44dcd297d8e4e2768ec1cdacb4d8b7434483717b0bc6f6a939662b3c253c68bcdfbe6966deb61ccc7db9c5c83c0535e390435802558dcd7c45bc'
